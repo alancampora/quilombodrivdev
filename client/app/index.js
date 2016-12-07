43895ad9b50b34d7151a826de9173c08dd840f1d
Alan Campora <alan.campora@gmail.com>
2016-12-07 00:20:50 -0300

    [ADD] layout code

---
+++ client/app/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Layout from './components/Layout';
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, app);

33806b7d9b02908b20a64037297e22133dfacaaa
Alan Campora <alan.campora@gmail.com>
2016-12-13 16:08:25 -0300

    [MOD] index linting issues

--- client/app/index.js
+++ client/app/index.js
@@ -1,14 +1,14 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './containers/App';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import {fetchPosts} from './actions/actions'
-import reducer from './reducers/index'
-import configureStore  from './store/store'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import {fetchPosts} from './actions/actions';
+import reducer from './reducers/index';
+import configureStore  from './store/store';
 
-let store = configureStore() 
-store.dispatch(fetchPosts())
+let store = configureStore(); 
+store.dispatch(fetchPosts());
 
 const app = document.getElementById('app');
 
@@ -16,4 +16,4 @@ ReactDOM.render(
     <Provider store={store}>
         <App/>
     </Provider>
-, app);
+,app);

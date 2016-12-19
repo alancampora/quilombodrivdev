3a2490159d16272755b6fcc053aca66c3a601272
Alan Campora <alan.campora@gmail.com>
2016-12-18 23:14:09 -0300

    [MOD] https for post request

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -26,7 +26,7 @@ export default {
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production'),
-                'API_HOST': JSON.stringify('http://qddback-labsjs.rhcloud.com/')
+                'API_HOST': JSON.stringify('https://qddback-labsjs.rhcloud.com/')
             }
         })
     ],

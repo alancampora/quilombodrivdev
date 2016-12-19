32fc10d598ef07660d7a310e01177a4d0e5d37b9
Alan Campora <alan.campora@gmail.com>
2016-12-18 22:53:15 -0300

    [MOD] sendfile

--- tools/postbuild.js
+++ tools/postbuild.js
@@ -15,7 +15,7 @@ const app = express();
  *
  */
 app.get('*', function(req, res) {  
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 app.listen(port, function(err) {  
   if (err) {
